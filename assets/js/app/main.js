8d907c82de3147d61de95bc597f9748b469b4248
Michael Chadwick <michael.chadwick@gmail.com>
2024-08-15 11:21:28 -0700

    improved help modal

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -26,34 +26,40 @@ Deckdle.modalOpen = async function (type) {
   switch (type) {
     case 'start':
     case 'help':
+      modalText = `
+        <p><strong>Deckdle</strong> is a <s>daily</s> solitaire card game. Currently, the only solitaire type is 'golf', but there are plans to add the classic 'klondike', and possibly 'pyramid' or 'spider'.</p>
+      `
+
       const gameType = Deckdle.__getState()['gameType']
       switch (gameType) {
         case 'golf':
         default:
-          modalText = `
-            <p>Deckdle is a daily solitaire card game. Currently, the only solitaire type is 'golf', but there are plans to add the classic 'klondike', and possibly 'pyramid' or 'spider'.</p>
-
+          modalText += `
             <h3 class="blamph">${gameType}</h3>
             <p>Exhaust the tableau onto your base before your stock runs out. You can move any unobscured card (just click/touch it) as long as its rank (e.g. 2, 8, J, etc.) is <strong>one higher</strong> or <strong>one lower</strong> than the current base card. Suit <strong>does not</strong> matter.</p>
-
-            <div class="flex">
-              <div>
-                <h4>Daily</h4>
-                <p>Come back every day (at 12 am PST) for a new <span class="blamph">${gameType}</span> solitaire tableau and stock!</p>
-              </div>
-
-              <div>
-                <h4>Free</h4>
-                <p>Play <span class="blamph">${gameType}</span> solitaire endlessly!
-              </div>
-            </div>
-
-            <hr />
-
-            <p><strong>Dev</strong>: <a href="https://michaelchadwick.info" target="_blank">Michael Chadwick</a>.</p>
           `
           break
       }
+
+      modalText += `
+        <div class="flex">
+          <s>
+            <div>
+              <h4>Daily</h4>
+              <p>Come back every day (at 12 am PST) for a new <span class="blamph">${gameType}</span> solitaire tableau and stock!</p>
+            </div>
+          </s>
+
+          <div>
+            <h4>Free</h4>
+            <p>Play <span class="blamph">${gameType}</span> solitaire endlessly!
+          </div>
+        </div>
+
+        <hr />
+
+        <p><strong>Dev</strong>: <a href="https://michaelchadwick.info" target="_blank">Michael Chadwick</a>.</p>
+      `
 
       this.myModal = new Modal(
         'perm',
