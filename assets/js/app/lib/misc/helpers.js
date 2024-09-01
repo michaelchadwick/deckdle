7a8e3c9a1e7b4aeea2245e5583edae12ea63b73f
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-01 00:09:31 -0700

    added a helpful(?) sleep promise-based function

--- assets/js/app/lib/misc/helpers.js
+++ assets/js/app/lib/misc/helpers.js
@@ -2,6 +2,10 @@
 /* misc global functions */
 /* global Deckdle */
 
+Deckdle._sleep = (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 // timestamp -> display date
 Deckdle.__getFormattedDate = function (date) {
   let formatted_date = ''
