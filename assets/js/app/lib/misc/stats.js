918abe008963daa7be1576d62bef1e6ef23ec89f
Michael Chadwick <michael.chadwick@gmail.com>
2024-09-03 07:42:45 -0700

    fixed var name

--- assets/js/app/lib/misc/stats.js
+++ assets/js/app/lib/misc/stats.js
@@ -3,11 +3,10 @@
 /* global Deckdle */
 
 Deckdle._getGameCount = function (mode) {
-  let ls = mode == 'daily' ?
-    localStorage.getItem(BOGDLE_STATE_DAILY_LS_KEY) :
-    localStorage.getItem(BOGDLE_STATE_FREE_LS_KEY)
+  let ls =
+    mode == 'daily'
+      ? localStorage.getItem(DECKDLE_STATE_DAILY_LS_KEY)
+      : localStorage.getItem(DECKDLE_STATE_FREE_LS_KEY)
 
-  return ls ?
-    JSON.parse(ls).filter((session) => session.lastCompletedTime).length :
-    0
+  return ls ? JSON.parse(ls).filter((session) => session.lastCompletedTime).length : 0
 }
