ed62c54080c1fa4ed8a237b8efd07d5a13e889e2
Michael Chadwick <michael.chadwick@gmail.com>
2024-10-24 14:35:20 -0700

    js lib reorg...again

--- assets/js/app/constants.js
+++ assets/js/app/constants.js
@@ -1,6 +1,16 @@
-/* lib/constants/specific */
-/* constant values: specific */
+/* lib/constants */
+/* constant values */
 /* eslint-disable no-unused-vars */
+
+const NEBYOOAPPS_SOURCE_URL = 'https://dave.neb.host/?sites'
+
+const DECKDLE_STATE_DAILY_LS_KEY = 'deckdle-state-daily'
+const DECKDLE_STATE_FREE_LS_KEY = 'deckdle-state-free'
+const DECKDLE_SETTINGS_LS_KEY = 'deckdle-settings'
+
+const DECKDLE_DAILY_SCRIPT = '/assets/php/daily.php'
+
+const DECKDLE_SHARE_URL = `${document.location.origin}/?r=share`
 
 const DECKDLE_GOLF_ROW_MAX = 5
 const DECKDLE_GOLF_COL_MAX = 7
