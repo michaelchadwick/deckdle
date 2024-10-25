47966581025f7bed5445379f525455d1caaa0ae8
Michael Chadwick <michael.chadwick@gmail.com>
2024-10-25 13:45:11 -0700

    updated combo test to not check multiple card combos yet cuz broken

--- cypress/integration/00-basic/04-combo.cy.js
+++ cypress/integration/00-basic/04-combo.cy.js
@@ -1,7 +1,15 @@
 context('00-basic', () => {
   context('combo', () => {
+    const COL_CARD_MAX = 5
+
     beforeEach(() => {
       cy.visit(Cypress.config().baseUrl)
+
+      // Turn on comboCounter so we can test it
+      let settings = JSON.parse(localStorage.getItem('deckdle-settings'))
+      settings.comboCounter = true
+      localStorage.setItem('deckdle-settings', JSON.stringify(settings))
+
       cy.get('#combo').as('combo')
       cy.get('#combo .text').as('comboMult')
       cy.get('#tableau #col0 .card.available').as('validCol0Card')
@@ -18,9 +26,10 @@ context('00-basic', () => {
       cy.get('@combo').should('not.be.visible')
     })
 
-    xit('should show x2 combo meter when a combo of 2 cards is achieved', () => {
-      cy.get('@validCol0Card').click()
-      cy.get('@validCol0Card').click({ force: true })
+    xit('should show correct x2 multiplier when two valid cards are played in a row', () => {
+      for (let i = 0; i < 2; i++) {
+        cy.get('@validCol0Card').click({ force: true, multiple: true })
+      }
       cy.get('@topBaseCard')
         .should('have.attr', 'data-suit', '2')
         .should('have.attr', 'data-rank', '4')
@@ -29,18 +38,90 @@ context('00-basic', () => {
       cy.get('@comboMult').should('have.text', 'x2')
     })
 
-    xit('should show x5 combo meter when a combo of 5 cards is achieved', () => {
+    xit('should show correct x(n*5) multiplier when an appropriate combo is achieved', () => {
+      // exhaust col0
       cy.get('@validCol0Card').click()
-      cy.get('@validCol0Card').click({ force: true })
-      cy.get('@validCol0Card').click({ force: true })
-      cy.get('@validCol0Card').click({ force: true })
-      cy.get('@validCol0Card').click({ force: true })
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol0Card').click({ force: true, multiple: true })
+      }
       cy.get('@topBaseCard')
         .should('have.attr', 'data-suit', '2')
         .should('have.attr', 'data-rank', '7')
 
       cy.get('@combo').should('be.visible')
       cy.get('@comboMult').should('have.text', 'x5')
+
+      // exhaust col1
+      cy.get('@validCol1Card').click()
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol1Card').click({ force: true, multiple: true })
+      }
+      cy.get('@topBaseCard')
+        .should('have.attr', 'data-suit', '2')
+        .should('have.attr', 'data-rank', '12')
+
+      cy.get('@combo').should('be.visible')
+      cy.get('@comboMult').should('have.text', 'x10')
+
+      // exhaust col2
+      cy.get('@validCol2Card').click()
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol2Card').click({ force: true, multiple: true })
+      }
+      cy.get('@topBaseCard')
+        .should('have.attr', 'data-suit', '3')
+        .should('have.attr', 'data-rank', '4')
+
+      cy.get('@combo').should('be.visible')
+      cy.get('@comboMult').should('have.text', 'x15')
+
+      // exhaust col3
+      cy.get('@validCol3Card').click()
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol3Card').click({ force: true, multiple: true })
+      }
+      cy.get('@topBaseCard')
+        .should('have.attr', 'data-suit', '3')
+        .should('have.attr', 'data-rank', '9')
+
+      cy.get('@combo').should('be.visible')
+      cy.get('@comboMult').should('have.text', 'x20')
+
+      // exhaust col4
+      cy.get('@validCol4Card').click()
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol4Card').click({ force: true, multiple: true })
+      }
+      cy.get('@topBaseCard')
+        .should('have.attr', 'data-suit', '3')
+        .should('have.attr', 'data-rank', '14')
+
+      cy.get('@combo').should('be.visible')
+      cy.get('@comboMult').should('have.text', 'x25')
+
+      // exhaust col5
+      cy.get('@validCol5Card').click()
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol5Card').click({ force: true, multiple: true })
+      }
+      cy.get('@topBaseCard')
+        .should('have.attr', 'data-suit', '0')
+        .should('have.attr', 'data-rank', '6')
+
+      cy.get('@combo').should('be.visible')
+      cy.get('@comboMult').should('have.text', 'x30')
+
+      // exhaust col6
+      cy.get('@validCol6Card').click()
+      for (let i = 0; i < COL_CARD_MAX - 1; i++) {
+        cy.get('@validCol6Card').click({ force: true, multiple: true })
+      }
+      cy.get('@topBaseCard')
+        .should('have.attr', 'data-suit', '0')
+        .should('have.attr', 'data-rank', '11')
+
+      cy.get('@combo').should('be.visible')
+      cy.get('@comboMult').should('have.text', 'x35')
     })
   })
 })
