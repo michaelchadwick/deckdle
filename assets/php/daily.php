49f6c1c40d0a8835ce8cc2db12010476bf821ecc
Michael Chadwick <michael.chadwick@gmail.com>
2024-11-12 14:01:19 -0800

    ok actually maybe finally fixed daily early rollover bug

--- assets/php/daily.php
+++ assets/php/daily.php
@@ -7,22 +7,22 @@ $tz = (new DateTimeZone('America/Los_Angeles'));
 $epochDateTime = new DateTime('2024-08-26', $tz);
 $serverDateTime = new DateTime('now', $tz);
 
-// daily puzzle index is equal to number of days since epoch
-$index = $epochDateTime->diff($serverDateTime)->format('%a');
+// daily puzzle index == days since epoch (int)
+$index = intval($epochDateTime->diff($serverDateTime)->format('%a'));
 
-// get unique date string for today
-$today = date($DATE_FORMAT, $serverDateTime->getTimestamp());
+// get unique date string for today (int-> string)
+$today = $serverDateTime->format($DATE_FORMAT);
 
-// get integer hash for today
+// get integer hash for today (hex)
 $hashHex = hash($HASH_ALGO, strval($today));
 
 // get today's setupId (hex -> int)
-$setupId = base_convert($hashHex, 16, 10);
+$setupId = intval(base_convert($hashHex, 16, 10));
 
 echo json_encode(array(
   'index' => $index,
   'message' => 'Got daily setupId and index',
   'setupId' => $setupId,
-  'status' => 'ok'
+  'status' => 'ok',
 ));
 ?>
