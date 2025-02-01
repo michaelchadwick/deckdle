6b2a14c1fb50fb42291cac5236f876740800f472
Michael Chadwick <michael.chadwick@gmail.com>
2025-02-01 12:20:24 -0800

    added _valType() helper method

--- assets/js/app/lib/helpers.js
+++ assets/js/app/lib/helpers.js
@@ -6,6 +6,41 @@ Deckdle._sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
+Deckdle._valType = (value) => {
+  if (value === null) {
+    return 'null'
+  }
+
+  const baseType = typeof value
+  // Primitive types
+  if (!['object', 'function'].includes(baseType)) {
+    return baseType
+  }
+
+  // Symbol.toStringTag often specifies the "display name" of the
+  // object's class. It's used in Object.prototype.toString().
+  const tag = value[Symbol.toStringTag]
+  if (typeof tag === 'string') {
+    return tag
+  }
+
+  // If it's a function whose source code starts with the "class" keyword
+  if (baseType === 'function' && Function.prototype.toString.call(value).startsWith('class')) {
+    return 'class'
+  }
+
+  // The name of the constructor; for example `Array`, `GeneratorFunction`,
+  // `Number`, `String`, `Boolean` or `MyCustomClass`
+  const className = value.constructor.name
+  if (typeof className === 'string' && className !== '') {
+    return className
+  }
+
+  // At this point there's no robust way to get the type of value,
+  // so we use the base implementation.
+  return baseType
+}
+
 // timestamp -> display date
 Deckdle.__getFormattedDate = (date) => {
   let formatted_date = ''
