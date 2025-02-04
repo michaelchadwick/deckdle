c042354495477e555f63beac9765db4f40a78df9
Michael Chadwick <michael.chadwick@gmail.com>
2025-02-04 09:12:24 -0800

    added show() method to StockAction class

--- assets/js/app/lib/classes/actions/stock_action.js
+++ assets/js/app/lib/classes/actions/stock_action.js
@@ -3,8 +3,13 @@
 /* eslint-disable no-unused-vars */
 
 class StockAction extends Action {
-  constructor() {
+  constructor(card) {
     super()
     this.type = 'stock'
+    this.card = card
+  }
+
+  show = () => {
+    return `${this.constructor.name.toString()}(${this.card.show()})`
   }
 }
